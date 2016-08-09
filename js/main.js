c4495de366ffa43e93a2359e6eeaa4f352894856
rigamarole <bonjour@rodolphe-besnier.com>
2016-08-09 17:40:33 +0200

    Nettoyage du fichier main.js

--- js/main.js
+++ js/main.js
@@ -12,10 +12,10 @@ var LeafIcon = L.Icon.extend({
 	}
 });
 
-var greenIcon = new LeafIcon({iconUrl: 'img/boston_celtics.svg'}),
-	redIcon = new LeafIcon({iconUrl: 'img/portland_trailblazers.svg'}),
-	orangeIcon = new LeafIcon({iconUrl: 'img/san_antonio_spurs.svg'});
+var bostonIcn = new LeafIcon({iconUrl: 'img/boston_celtics.svg'}),
+	portlandIcn = new LeafIcon({iconUrl: 'img/portland_trailblazers.svg'}),
+	SanAntonioIcn = new LeafIcon({iconUrl: 'img/san_antonio_spurs.svg'});
 
-L.marker([42.357778, -71.061667], {icon: greenIcon}).bindPopup("Boston Celtics").addTo(map);
-L.marker([45.516667, -122.666667], {icon: redIcon}).bindPopup("Portland Trailblazers").addTo(map);
-L.marker([29.428611, -98.493333], {icon: orangeIcon}).bindPopup("San Antonio Spurs").addTo(map);
+L.marker([42.357778, -71.061667], {icon: bostonIcn}).bindPopup("Boston Celtics").addTo(map);
+L.marker([45.516667, -122.666667], {icon: portlandIcn}).bindPopup("Portland Trailblazers").addTo(map);
+L.marker([29.428611, -98.493333], {icon: SanAntonioIcn}).bindPopup("San Antonio Spurs").addTo(map);
